3d284d8b976020a1ef8e3a6647207a6565342630
Zach Nicoll <zach.nicoll55@gmail.com>
2021-01-17 21:58:35 +1000

    updated help message

--- Main.fs
+++ Main.fs
@@ -13,23 +13,39 @@ let runMigration connStr =
     |> Sql.executeNonQuery
 
 let helpMessage () =
-    Logger.info "Migrations can be run with the following arguments:"
-    Logger.info "\tfazor --host localhost --port 5432 --user username --pass password --database my_db"
+    Logger.info """
+    fazor upgrade --host localhost --port 5432 --user username --pass password --database my_db
+
+    ARG                 | DESCRIPTION
+    --------------------------------------------------------
+    init                | setup fazor in current directory
+    --------------------------------------------------------
+    upgrade             | run up to latest migration
+        |_ --host       |   DB host address (optional)
+        |_ --port       |   DB host port (optional)
+        |_ --user       |   DB connection username
+        |_ --pass       |   DB connection password
+        |_ --database   |   DB name
+    --------------------------------------------------------
+    help                | show help
+    --------------------------------------------------------
+    """
 
 
 [<EntryPoint>]
 let main argv =
     match (argv |> Array.toList) with
-    | [] -> Logger.error "No arguments found! Use --h to get some help."
-    | ["--h"] -> helpMessage()
-    | _args ->
+    | [] -> Logger.error "No arguments found! Run fazor help to get some help."
+    | ["help"] -> helpMessage()
+    | ["init"] -> ()
+    | "upgrade"::_args ->
         let args:ArgParser.CommandLineOptions = ArgParser.parseCommandLine _args ArgParser.DefaultOptions 
 
         if args.Database.Equals "" || args.Username.Equals "" || args.Password.Equals "" then
-            Logger.error "Missing required arguments \'--database\', \'--user\' or \'--pass\'"
+            Logger.error "Missing required argument(s) \'--database\', \'--user\' and \'--pass\'"
             helpMessage()
         else
-            Logger.info $"Performing migration on {args.Host}:{args.Port}@{args.Database} as \"{args.Username}\"..."
+            Logger.info $"Performing migration on postgresql://{args.Host}:{args.Port}/{args.Database} as \"{args.Username}\"..."
 
             let connStr =
                 Sql.host args.Host
@@ -42,5 +58,6 @@ let main argv =
             match runMigration connStr with
             | Ok _ -> Logger.ok $"Migration succeeded!"
             | Error e -> Logger.error $"Migration failed! Here's the stacktrace: \n\n{e}\n"
+    | _ -> Logger.error "No recognisable arguments found. Run fazor help to get some help."
     
     0
