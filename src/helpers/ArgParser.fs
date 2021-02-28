37b1ca5d0f202231c97f5d6e358e13f0e83c4a85
Zach Nicoll <zach.nicoll55@gmail.com>
2021-02-28 20:36:33 +1000

    feat: beginning of env vars

--- src/helpers/ArgParser.fs
+++ src/helpers/ArgParser.fs
@@ -1,19 +1,26 @@
 namespace Fazor
 
 module ArgParser =
+    open System
     type CommandLineOptions =
-        { Host: string
-          Port: int
-          Username: string
-          Password: string
-          Database: string }
+        { mutable Host: string option
+          mutable Port: int option
+          mutable Username: string option
+          mutable Password: string option
+          mutable Database: string option }
 
     let DefaultOptions: CommandLineOptions =
-        { Host = "localhost"
-          Port = 5432
-          Username = ""
-          Password = ""
-          Database = "" }
+        { Host = None
+          Port = None
+          Username = None
+          Password = None
+          Database = None }
+    
+    let envDb () = "FAZOR_DB"
+    let envUsername () = "FAZOR_USERNAME"
+    let envPassword () = "FAZOR_PASSWORD"
+    let envHost () = "FAZOR_HOST"
+    let envPort () = "FAZOR_PORT"
 
     let sliceRest (l: list<string>) =
         match l.Length with
@@ -34,31 +41,31 @@ module ArgParser =
                 parseCommandLine
                     (sliceRest rest)
                     { optionsSoFar with
-                          Host = rest.[0] }
+                          Host = Some(rest.[0]) }
 
             | "--port" :: rest ->
                 parseCommandLine
                     (sliceRest rest)
                     { optionsSoFar with
-                          Port = rest.[0] |> int}
+                          Port = Some(rest.[0] |> int)}
 
             | "--user" :: rest ->
                 parseCommandLine
                     (sliceRest rest)
                     { optionsSoFar with
-                          Username = rest.[0] }
+                          Username = Some(rest.[0]) }
 
             | "--pass" :: rest ->
                 parseCommandLine
                     (sliceRest rest)
                     { optionsSoFar with
-                          Password = rest.[0] }
+                          Password = Some(rest.[0]) }
 
             | "--database" :: rest ->
                 parseCommandLine
                     (sliceRest rest)
                     { optionsSoFar with
-                          Database = rest.[0] }
+                          Database = Some(rest.[0]) }
 
             // handle unrecognized option and keep looping
             | x :: rest ->
@@ -68,3 +75,48 @@ module ArgParser =
             | :? System.ArgumentException -> 
                 Logger.warn "You supplied an option with no value!"
                 optionsSoFar
+
+    let nullCheck var =
+        if isNull var then
+            None
+        else
+            Some(var)
+    
+    let nullCheckDefault defaultVal var =
+        if isNull var then
+            Some(defaultVal)
+        else
+            Some(var)
+    
+    let checkEnvVars args =
+        match args.Database with
+        | None -> 
+            args.Database <- Environment.GetEnvironmentVariable(envDb ())
+            |> nullCheck
+        | _ -> ()
+
+        match args.Username with
+        | None -> args.Username <- Environment.GetEnvironmentVariable(envUsername ()) |> nullCheck
+        | _ -> ()
+
+        match args.Password with
+        | None -> args.Password <- Environment.GetEnvironmentVariable(envPassword ()) |> nullCheck
+        | _ -> ()
+
+        match args.Host with
+        | None -> args.Host <- Environment.GetEnvironmentVariable(envHost ()) |> nullCheckDefault "localhost"
+        | _ -> ()
+
+        match args.Port with
+        | None ->
+            args.Port <-
+                    Environment.GetEnvironmentVariable(envPort ())
+                    |> nullCheckDefault "5432"
+                    |> fun(port) -> 
+                        if (port.IsSome) then 
+                            Some(port.Value |> int)
+                        else
+                            None
+        | _ -> ()
+
+        args
--- src/helpers/Connect.fs
+++ src/helpers/Connect.fs
@@ -1,34 +1,37 @@
 namespace Fazor
 
 module Connect =
-  open Npgsql.FSharp
-  open ArgParser
+    open System
+    open Npgsql.FSharp
+    open ArgParser
 
-  let makeConnection _args =
-    parseCommandLine _args DefaultOptions
-    |> fun args ->
-        match (args.Database.Equals ""
-               || args.Username.Equals ""
-               || args.Password.Equals "") with
-        | false ->
-            Logger.info
-                $"Connecting to postgresql://{args.Host}:{args.Port}/{args.Database} as '{args.Username}'..."
+    let makeConnection _args =
+        parseCommandLine _args DefaultOptions
+        |> checkEnvVars
+        |> fun args ->
 
-            Some(
-                Sql.host args.Host
-                |> Sql.database args.Database
-                |> Sql.username args.Username
-                |> Sql.password args.Password
-                |> Sql.port args.Port
-                |> Sql.formatConnectionString
-            )
-        | true ->
-            Logger.error "Missing required argument(s) \'--database\', \'--user\' and \'--pass\'"
-            None
+            match (args.Database = None
+                   || args.Username = None
+                   || args.Password = None) with
+            | false ->
+                Logger.info
+                    $"Connecting to postgresql://{args.Host}:{args.Port}/{args.Database} as '{args.Username}'..."
 
-  let connectToClient conn =
-      match conn with
-      | Some conn -> Sql.connect conn
-      | None ->
-          Logger.error "Could not make a connection to the database, check the error above."
-          failwith "Connection failed!"
+                Some(
+                    Sql.host args.Host.Value
+                    |> Sql.database args.Database.Value
+                    |> Sql.username args.Username.Value
+                    |> Sql.password args.Password.Value
+                    |> Sql.port args.Port.Value
+                    |> Sql.formatConnectionString
+                )
+            | true ->
+                Logger.error "Missing required argument(s) \'--database\', \'--user\' and \'--pass\'"
+                None
+
+    let connectToClient conn =
+        match conn with
+        | Some conn -> Sql.connect conn
+        | None ->
+            Logger.error "Could not make a connection to the database, check the error above."
+            failwith "Connection failed!"
